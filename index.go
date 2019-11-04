1780ed7d4d67f0a175876177a8097dd9455da33d
panafeng <yinxinpz@163.com>
2019-11-04 21:47:54 +0800

    simple example

---
+++ index.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"./tools"
+)
+
+func main() {
+	fmt.Println("demo")
+	tools.MyPrint("这是tools包中的方法")
+}
---
+++ tools/print.go
@@ -0,0 +1,6 @@
+package tools
+
+import "fmt"
+func MyPrint(str string){
+	fmt.Println(str)
+}
