c8118bfcce9984a1ba039dd81d06f6f261b8dea8
panafeng <yinxinpz@163.com>
2020-01-07 00:24:18 +0800

    init develop

--- src/go_dev/http_simple_server/httpSimpleServer.go
+++ src/go_dev/http_simple_server/httpSimpleServer.go
@@ -1,11 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello this is my first go server"))
+	w.Write([]byte("hello, I am a simple server by golang!"))
 }
+
 func main() {
+	fmt.Println("服务启动在:127.0.0.1:8001地址上")
 	http.HandleFunc("/", handler)
 	http.ListenAndServe("127.0.0.1:8001", nil)
+
 }
